8830ebe9f69597aa53b0f0424f5d8dbf9d30b8bf
Daniel Burrell <daniel.burrell@gmail.com>
2015-06-19 21:04:42 +0100

    rootconfiguration
    rootconfiguration annotation finalised

--- src/main/java/uk/co/solong/application/main/SpringAutoAnnotationContextApplication.java
+++ src/main/java/uk/co/solong/application/main/SpringAutoAnnotationContextApplication.java
@@ -1,9 +1,17 @@
 package uk.co.solong.application.main;
 
+import java.util.List;
+import java.util.Set;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
+import org.springframework.core.type.filter.AnnotationTypeFilter;
+import org.springframework.util.StringUtils;
+
+import uk.co.solong.application.annotations.RootConfiguration;
 
 /**
  * Generic application harness for Java-Config Annotation based applications. To
@@ -15,19 +23,45 @@ import org.springframework.context.annotation.ClassPathScanningCandidateComponen
 public class SpringAutoAnnotationContextApplication {
     private static final Logger logger = LoggerFactory.getLogger(SpringAutoAnnotationContextApplication.class);
 
-    public void run(String configClass) {
+    public void run(String qualifiedRootConfiguration) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
 
         boolean started = false;
         try {
-              
-            ClassLoader classLoader = SpringAutoAnnotationContextApplication.class.getClassLoader();
-            Class<?> aClass = classLoader.loadClass(configClass);
-            context.register(aClass);
-            context.registerShutdownHook();
-            context.refresh();
-            logger.info("Application started");
-            started = true;
+            logger.info("Scanning for root configuration");
+            ClassPathScanningCandidateComponentProvider scanner = new ClassPathScanningCandidateComponentProvider(false);
+
+            scanner.addIncludeFilter(new AnnotationTypeFilter(RootConfiguration.class));
+
+            
+            Set<BeanDefinition> bd = scanner.findCandidateComponents("");
+        
+            
+            if (bd.size() == 1) {
+                logger.info("RootConfiguration found");
+                String value = bd.iterator().next().getBeanClassName();
+                started = startApp(context, value);
+            } else if (bd.size() > 1) {
+                logger.info("Multiple RootConfigurations found");
+                if (StringUtils.isEmpty(qualifiedRootConfiguration)) {
+                    throw new RuntimeException("Multiple RootConfigurations found, but no qualifier specified");
+                }
+                boolean found = false;
+                while (bd.iterator().hasNext()){
+                    String value = bd.iterator().next().getBeanClassName();
+                    RootConfiguration r = Class.forName(value).getAnnotation(RootConfiguration.class);
+                    if (qualifiedRootConfiguration.equals(r.name())){
+                        found = true;
+                        started = startApp(context, value);
+                        break;
+                    }
+                }
+                if (!found) {
+                    throw new RuntimeException("Multiple RootConfigurations found, but none match the name: "+qualifiedRootConfiguration);
+                }
+            } else if (bd.size() == 0) {
+                throw new RuntimeException("Must have at least 1 Configuration class annotated with @RootConfiguration on the classpath");
+            }
         } catch (RuntimeException e) {
             throw new RuntimeException("Application failed to start", e);
         } catch (Exception e) {
@@ -41,6 +75,18 @@ public class SpringAutoAnnotationContextApplication {
                 }
             }
         }
+    }
+
+    private boolean startApp(AnnotationConfigApplicationContext context, String value) throws ClassNotFoundException {
+        boolean started;
+        ClassLoader classLoader = SpringAutoAnnotationContextApplication.class.getClassLoader();
+        Class<?> aClass = classLoader.loadClass(value);
+        context.register(aClass);
+        context.registerShutdownHook();
+        context.refresh();
+        logger.info("Application started");
+        started = true;
+        return started;
     }
 
     public static void main(String[] args) {
